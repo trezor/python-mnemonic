648f972a1e941e406bd6050e3b3cd754c1c48895
Pavol Rusnak <stick@gk2.sk>
2015-06-02 21:45:23 +0200

    make shamir code accept all possible lengths

--- mnemonic/shamir.py
+++ mnemonic/shamir.py
@@ -23,49 +23,41 @@ from mnemonic import Mnemonic
 from secretsharing import secret_int_to_points, points_to_secret_int
 import binascii
 
-# see https://primes.utm.edu/lists/2small/ for biggest primes that fit into X bits
-
-# 12 words
-# DATALEN = (16 - 1)       # 120 bits
-# PRIME   = (2**120 - 119) # biggest prime that fits into 120 bits
-
-# 15 words
-DATALEN = (20 - 1)       # 152 bits
-PRIME   = (2**152 - 17)  # biggest prime that fits into 152 bits
-
-# 18 words
-# DATALEN = (24 - 1)       # 184 bits
-# PRIME   = (2**184 - 33)  # biggest prime that fits into 184 bits
-
-# 21 words
-# DATALEN = (28 - 1)       # 216 bits
-# PRIME   = (2**216 - 377) # biggest prime that fits into 216 bits
-
-# 24 words
-# DATALEN = (32 - 1)       # 248 bits
-# PRIME   = (2**248 - 237) # biggest prime that fits into 248 bits
-
 class Shamir(object):
 
 	def __init__(self, language):
 		self.mnemo = Mnemonic(language)
+		# see https://primes.utm.edu/lists/2small/ for biggest primes that fit into X bits
+		self.primes = {
+			15: (2**120 - 119),
+			19: (2**152 - 17),
+			23: (2**184 - 33),
+			27: (2**216 - 377),
+			31: (2**248 - 237)
+		}
 
 	def split(self, data, m, n):
-		if len(data) != DATALEN:
-			raise Exception('Data length should be %d bits, not %d bits.' % (DATALEN * 8, len(data) * 8))
+		if not len(data) in self.primes.keys():
+			raise Exception('Unknown data length')
 		if m < 2 or m > 15:
 			raise Exception('Invalid M provided')
 		if n < 2 or n > 15:
 			raise Exception('Invalid N provided')
-		s = secret_int_to_points(int(binascii.hexlify(data), 16), m, n, PRIME)
-		s = [ '%x%x%s' % (m, x[0], ('%x' % x[1]).zfill(DATALEN * 2)) for x in s ]
+		prime = self.primes[len(data)]
+		s = secret_int_to_points(int(binascii.hexlify(data), 16), m, n, prime)
+		s = [ '%x%x%s' % (m, x[0], ('%x' % x[1]).zfill(len(data) * 2)) for x in s ]
 		return [ self.mnemo.to_mnemonic(binascii.unhexlify(x)) for x in s ]
 
 	def combine(self, shares):
+		words = set([ len(x.split(' ')) for x in shares ])
+		if len(words) != 1:
+			raise Exception('Inconsistent number of words')
+		datalen = list(words)[0] * 4 / 3 - 1
 		shares = [ binascii.hexlify(self.mnemo.to_entropy(x)) for x in shares ]
 		if set([ int(x[0], 16) for x in shares ]) != set([len(shares)]):
 			raise Exception('Number of shares does not match the threshold')
 		points = [ ( int(x[1], 16), int(x[2:], 16) ) for x in shares ]
-		r = points_to_secret_int(points, PRIME)
-		r = hex(r)[2:-1].zfill(DATALEN * 2)
+		prime = self.primes[datalen]
+		r = points_to_secret_int(points, prime)
+		r = hex(r)[2:-1].zfill(datalen * 2)
 		return binascii.unhexlify(r)
--- mnemonic/test_shamir.py
+++ mnemonic/test_shamir.py
@@ -2,9 +2,9 @@
 from shamir import Shamir
 import sys
 
-def shamir_test(m, n):
+def shamir_test(l, m, n):
 	s = Shamir('english')
-	seed = "Shamir for Mnemonic"
+	seed = "Shamir's Secret Sharing Scheme!"[:l] # take first l characters
 	shares = s.split(seed, m, n)
 	print 'original:', seed
 	print 'shares:'
@@ -20,6 +20,7 @@ def shamir_test(m, n):
 		print 'TEST FAILED !!!'
 		sys.exit(1)
 
-for n in range(2, 15 + 1):
-	for m in range(2, n + 1):
-		shamir_test(m, n)
+for l in [15, 19, 23, 27, 31]:
+	for n in range(2, 15 + 1):
+		for m in range(2, n + 1):
+			shamir_test(l, m, n)
