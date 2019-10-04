39ee5e4484e97e2f16780167d50ecf47e1b9b63d
matejcik <ja@matejcik.cz>
2019-10-04 14:29:19 +0200

    add python markers, enable universal wheels

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+
+def read(*path):
+    filename = os.path.join(CWD, *path)
+    with open(filename, "r") as f:
+        return f.read()
+
 
 setup(
     name="mnemonic",
@@ -7,6 +17,7 @@ setup(
     author="Trezor",
     author_email="info@trezor.io",
     description="Implementation of Bitcoin BIP-0039",
+    long_description=read("README.rst"),
     url="https://github.com/trezor/python-mnemonic",
     packages=["mnemonic"],
     package_data={"mnemonic": ["wordlist/*.txt"]},
@@ -16,5 +27,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
 )
