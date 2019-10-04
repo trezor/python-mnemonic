38e05896769214ca20d040cdf9541311fc25b5ba
matejcik <ja@matejcik.cz>
2019-10-04 14:15:12 +0200

    drop empty requirements.txt, bump version

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 
 setup(
     name="mnemonic",
-    version="0.18",
-    author="Bitcoin TREZOR",
+    version="0.19",
+    author="Trezor",
     author_email="info@trezor.io",
     description="Implementation of Bitcoin BIP-0039",
     url="https://github.com/trezor/python-mnemonic",
     packages=["mnemonic"],
     package_data={"mnemonic": ["wordlist/*.txt"]},
     zip_safe=False,
-    install_requires=["pbkdf2"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
