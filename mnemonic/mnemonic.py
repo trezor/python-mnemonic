709c52e99ae05cdaf512a3d0e2847451d682820a
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-06-17 14:13:08 +0300

    Remove needless hex conversion

--- mnemonic/mnemonic.py
+++ mnemonic/mnemonic.py
@@ -211,7 +211,7 @@ class Mnemonic(object):
         l = len(b)  # noqa: E741
         d = b[: l // 33 * 32]
         h = b[-l // 33 :]
-        nd = bytes.fromhex(hex(int(d, 2))[2:].zfill(l // 33 * 8))
+        nd = int(d, 2).to_bytes(l // 33 * 4, byteorder="big")
         nh = bin(int(hashlib.sha256(nd).hexdigest(), 16))[2:].zfill(256)[: l // 33]
         return h == nh
 
