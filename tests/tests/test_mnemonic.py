f37b5cde504a5817c43090fcd661757c3126e32b
Seth Landry <waymakerijn@outlook.com>
2025-03-23 19:28:22 -0500

    Create test_mnemonic.py
    Create python-publish.yml
    
    Here is an example of a `python-publish.yml` GitHub Actions workflow file for publishing a Python package to PyPI:
    
    ```yaml
    name: Publish Python Package
    
    on:
      push:
        branches:
          - main
          - master
      pull_request:
        branches:
          - main
          - master
    
    jobs:
      build:
        runs-on: ubuntu-latest
    
        steps:
          - name: Checkout repository
            uses: actions/checkout@v2
    
          - name: Set up Python
            uses: actions/setup-python@v2
            with:
              python-version: '3.x'
    
          - name: Install dependencies
            run: |
              python -m pip install --upgrade pip
              pip install setuptools wheel
    
          - name: Build package
            run: |
              python setup.py sdist bdist_wheel
    
          - name: Publish package to PyPI
            env:
              TWINE_USERNAME: ${{ secrets.PYPI_USERNAME }}
              TWINE_PASSWORD: ${{ secrets.PYPI_PASSWORD }}
            run: |
              python -m pip install --upgrade twine
              twine upload dist/*

---
+++ tests/tests/test_mnemonic.py
@@ -0,0 +1,5 @@
+ImportError while importing test module '/home/runner/work/python-mnemonic/python-mnemonic/tests/test_mnemonic.py'.
+Hint: make sure your test modules/packages have valid Python names.
+tests/test_mnemonic.py:29: in <module>
+    from mnemonic import Mnemonic
+E   ModuleNotFoundError: No module named 'mnemonic'
